a2a6fa96d596121dcd35769002bf4e18f30d0c87
Emanuele discala <manueldiscala@gmail.com>
2023-07-05 12:00:14 +0200

    edit(comic.show,controller)

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -63,9 +63,9 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(comic $comic)
     {
-        //
+        return view("comics.show", compact("comic") );
     }
 
     /**
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,11 +2,12 @@
 
 @section('content')
 <div class="container my-3">
+    
     <h1>Dettagli: {{$comic->title}}</h1>
+        
     <div class="row g-4">
         <div class="col">
-            
-            <a href="{{ route("home") }}">Torna alla lista prodotti</a>
+            <a href="{{ route("comics.index") }}">Torna alla lista prodotti</a>
         </div>
     </div>
 
